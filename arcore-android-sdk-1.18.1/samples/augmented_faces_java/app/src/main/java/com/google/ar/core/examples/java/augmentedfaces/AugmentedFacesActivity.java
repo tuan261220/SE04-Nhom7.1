ecd6559b30e0a32c87dfc0edf2aad000e3163b03
Admin <lhx2911.pp@gmail.com>
2020-12-29 11:01:49 +0700

    second commit

--- arcore-android-sdk-1.18.1/samples/augmented_faces_java/app/src/main/java/com/google/ar/core/examples/java/augmentedfaces/AugmentedFacesActivity.java
+++ arcore-android-sdk-1.18.1/samples/augmented_faces_java/app/src/main/java/com/google/ar/core/examples/java/augmentedfaces/AugmentedFacesActivity.java
@@ -220,15 +220,7 @@ public class AugmentedFacesActivity extends AppCompatActivity implements GLSurfa
           "models/hoke.jpg");
       noseObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
       noseObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);
-//      rightEarObject.createOnGlThread(
-//          this, "models/FOREHEAD_RIGHT.obj", "models/ear_fur.png");
-//      rightEarObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
-//      rightEarObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);
-//      leftEarObject.createOnGlThread(
-//          this, "models/FOREHEAD_LEFT.obj", "models/ear_fur.png");
-//      leftEarObject.setMaterialProperties(0.0f, 1.0f, 0.1f, 6.0f);
-//      leftEarObject.setBlendMode(ObjectRenderer.BlendMode.AlphaBlending);
-
+      
     } catch (IOException e) {
       Log.e(TAG, "Failed to read an asset file", e);
     }
